84624c8b0f4958a763ca2c0db2cfd849cf130a28
lik03 <136569057+lik03@users.noreply.github.com>
2023-06-23 10:04:31 +0800

    Delete js directory

--- js/head.js
+++
@@ -1,9 +0,0 @@
-var navli = document.getElementsByClassName("navli")[0];
-var headTz = document.getElementsByClassName("head-tz")[0];
-
-navli.onmouseenter = function(){
-	headTz.style.height = "240px";
-};
-headTz.onmouseleave = function(){
-	headTz.style.height = "0px";
-};
